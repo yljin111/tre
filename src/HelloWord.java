be492c68b52da97027fcf9253d3f348bc8d4c7dc
yljin <yljin@isoftstone.com>
2019-12-26 16:54:51 +0800

    56156156

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+class HelloWord{
+public static void main(String []args){
+	int n = 12;
+	for (int i = 0; i < 1000; i++){
+		System.out.println("("+"'"+n+"'"+",'"+ n+"', 126, '2019.12.25', '2019.12.27', '1', '"+n+"', " +
+				"'企业申报品牌展位评审', 1, '1', '0', NULL, NULL, NULL, NULL, '9d5984fb537a4ceba757d9607d35c651', '46数量工作部', '2019-12-25 15:03:11'),");
+		n++;
+	}
+}
+}
